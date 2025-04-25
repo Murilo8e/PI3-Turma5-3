27471755a43c5779345ee1fd521cb7ae87d0863b
Raphael Moreira <rmoreira16@gmail.com>
2025-04-25 16:16:27 -0300

    Initial commit

---
+++ app/src/main/java/com/example/fireauthtest/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.fireauthtest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            mainScreen()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun mainScreen() {
+
+    Text(text = "Tela principal =)")
+
+}
