918ab1f4f8ac236c0d5096e72d5c28c1e1a5a6b7
Raphael Moreira <rmoreira16@gmail.com>
2025-05-04 23:14:32 -0300

    ajustes iniciais, gradle, manifest

--- app/src/main/java/com/example/fireauthtest/WelcomeActivity.kt
+++ app/src/main/java/com/example/fireauthtest/WelcomeActivity.kt
@@ -38,68 +38,7 @@ class WelcomeActivity : ComponentActivity() {
 @Composable
 fun welcomeScreen(modifier: Modifier = Modifier){
 
-    /* Usando o sistema de navegação legado
-       Através de intents.
-
-       A variável context recebe o objeto do contexto atual.
-       O LocalContext serve APENAS em Composables.
-
-       O Context agora informa o ambiente composable que se está.
-       Ele servirá para construir o Intent futuramente
-       (estou aqui (context) e quero ir para tal activity (classe da Activity))
-       
-    */
-
-    val context = LocalContext.current
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(5.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(
-            text = "Testando o Firebase Auth :)",
-            style = TextStyle(
-                fontSize = 25.sp
-            )
-        )
-
-        Spacer(modifier = Modifier.height(25.dp))
-
-        Button(
-            onClick = {
-                val intent = Intent(context, SignUpActivity::class.java)
-                context.startActivity(intent)
-            },
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color(0xFF374CCE)
-            )
-        ) {
-            Text(
-                text = "Faça seu Cadastro"
-            )
-        }
-
-        Spacer(modifier = Modifier.height(10.dp))
-
-        Button(
-            onClick = {
-                val intent = Intent(context, SignInActivity::class.java)
-                context.startActivity(intent)
-                },
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color(0xFF4ACC4E)
-            )
-        ) {
-            Text(
-                text = "Entrar"
-            )
-        }
-
-
-    }
+    
 }
 
 
